fdb3782a6889bbb11641e0c4f8cca75b310c39f4
Linuks <51865813+Linuks777@users.noreply.github.com>
2019-08-26 16:06:46 +0000

    Update cvmutils-catfork.meta.js

--- cvmutils-catfork.meta.js
+++ cvmutils-catfork.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name New Collab VM
-// @description (d1g1talcat's fork) Adds new features to Collab VM chat
-// @namespace CollabVM
-// @version 1.0.0.rc2
+// @description (d1g1talcat's fork, now the CollabVM Communites fork.) Adds new features to Collab VM chat
+// @license ISC; https://raw.githubusercontent.com/CollabVM-Unofficial/New-CollabVM/master/LICENSE
+// @namespace collabvm
+// @version 1.0.0-rc2
 // @include http://computernewb.com/*
 // @include http://malakas.ml:6004/*
 // @run-at document-end
 // @grant none
-// @updateURL https://github.com/CollabVM-Unofficial/New-CollabVM/raw/master/cvmutils-catfork.meta.js
-// @downloadURL https://github.com/CollabVM-Unofficial/New-CollabVM/raw/master/cvmutils-catfork.user.js
+// @updateURL https://raw.githubusercontent.com/CollabVM-Unofficial/New-CollabVM/master/cvmutils-catfork.meta.js
+// @downloadURL https://raw.githubusercontent.com/CollabVM-Unofficial/New-CollabVM/master/cvmutils-catfork.user.js
 // ==/UserScript==
